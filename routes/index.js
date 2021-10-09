78868e9de91b02e877a2e2d3294c7c002c074c49
L11223344 <john112233@live.ca>
2021-10-09 22:50:30 +0500

    first commit to portfilio website

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+// route for the main page
+router.get('/', function (req, res, next) {
+  res.render('home',{title:"Home",heading:"Welcome",para:"When you are emotionally and mentally invested in a goal, you are likely to have increased motivation Your mission statement helps you determine what you are passionate about and develop an understanding of what motivates you",
+button_head:"Click to see the About Page"});
+})
+
+// route for the home page
+router.get('/home', function (req, res, next) {
+  res.render('home',{title:"Home",heading:"Welcome",para:"When you are emotionally and mentally invested in a goal, you are likely to have increased motivation Your mission statement helps you determine what you are passionate about and develop an understanding of what motivates you",
+  button_head:"Click to see the About Page"});
+})
+
+let arr=[];
+
+// the post API for fetching form details
+router.post('/test', (req, res) => {
+  const body=req.body;
+ arr.push(body)
+  res.redirect('/home')
+})
+// route for the about page
+router.get('/about', function (req, res, next) {
+  res.render('about',{title:"About Me",para:"WHO YOU ARE!",desc:"Jhon is a WordPress developer and designer whose portfolio emphasizes her focus on customized, responsive website designs.I build beautiful layouts in using html bootstrap and good use of backend knoledge using node js",name:"Jhon Doe"});
+});
+
+// route for the project page
+router.get('/project', function (req, res, next) {
+  res.render('project',{title:"Project"});
+});
+
+// route for the service page
+router.get('/service', function (req, res, next) {
+  res.render('service',{title:"Services"});
+});
+const result=["FirstName,LastName,ContactNumber"]
+// route for the contact page
+router.get('/contact', function (req, res, next) {
+  res.render('contact',{title:"Contact",results:result});
+});
+module.exports = router;
