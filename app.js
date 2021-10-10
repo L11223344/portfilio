1d021aaa4894f7c920c901cc02fb4424496eb7d9
L11223344 <john112233@live.ca>
2021-10-11 01:45:39 +0500

    changes some styles

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var partials    = require('express-partials');
 var indexRouter = require('./routes/index');
 
 var app = express();
